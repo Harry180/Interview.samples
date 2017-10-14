5ed681f8c798509b9f63f3a6da35651e99d8ad5b
Lukasz Charubin <lukasz.charubin@gmail.com>
2017-10-14 14:19:41 +0200

    Add test which really check something not only existance of file.

--- tests/getValue.sample-spec.js
+++ tests/getValue.sample-spec.js
@@ -1,8 +1,10 @@
 'use strict';
 var expect = require('chai').expect;
-describe("getValue", function () {
-    it("should exist", function() {
-        var getValueFunc = require('./../getValue.sample.js');
-        expect(getValueFunc).to.not.be.undefined;
+var getValueFunc = require('./../getValue.sample.js');
+describe("when array passed have 2 or less elements", function () {
+    it("should return 0", function () {
+        var array = [1,2];
+        var result = getValueFunc(array);
+        expect(result).to.be.equal(0);
     });
 });
