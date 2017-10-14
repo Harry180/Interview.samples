9c132b953e76e04d3e67ddcdaa017ffdc17c0fc7
Lukasz Charubin <lukasz.charubin@gmail.com>
2017-10-14 14:37:06 +0200

    Add test for bigger than 2 elements array passed to function

--- tests/getValue.sample-spec.js
+++ tests/getValue.sample-spec.js
@@ -7,4 +7,19 @@ describe("when array passed have 2 or less elements", function () {
         var result = getValueFunc(array);
         expect(result).to.be.equal(0);
     });
+
+    it("should be different than 0", function () {
+        var array = [1,2,3];
+        var result = getValueFunc(array);
+        expect(result).to.not.be.equal(0);
+    });
+});
+
+describe("should return second smallest value when more than 2 elements in array", function(){
+    it("should pass", function (){
+        var array = [32, 99, 1, 5, 10, 30, 2];
+        var actual = getValueFunc(array);
+        var expected = array.sort(function(a, b) { return a-b;})[1];
+        expect(actual).to.be.equal(expected);
+    });
 });
